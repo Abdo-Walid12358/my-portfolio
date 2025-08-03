7689a9e54be7491644ad926f2d23241606eaf5d1
Abdelrahman Walid <134791898+Abdo-Walid12358@users.noreply.github.com>
2025-08-03 23:29:32 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import AboutSection from "@/components/sections/about-section";
 import ContactSection from "@/components/sections/contact-section";
 import HeroSection from "@/components/sections/hero-section";
 import ProjectsSection from "@/components/sections/projects-section";
@@ -10,6 +11,7 @@ export default function HomePage() {
         <>
             <Header />
             <HeroSection />
+            <AboutSection />
             <SkillsSection />
             <ProjectsSection />
             <ContactSection />
